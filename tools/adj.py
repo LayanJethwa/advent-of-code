0fc5352e2da07fd4f11b771ca418a7e7e2580fd1
LayanJethwa <84594346+LayanJethwa@users.noreply.github.com>
2023-12-11 07:05:06 +0000

    Update adj.py

--- tools/adj.py
+++ tools/adj.py
@@ -16,3 +16,33 @@ def a(ll,x,y,d):
             if not(i==1 and j==1) and ne[i+x][j+y] != d:
                 n.append(ne[i+x][j+y])
     return(n)
+
+def ac(ll,x,y,d): #w,n,e,s
+    global ne
+    ne = []
+    n = d+d
+    for i in range(len(str(ll[0]))):
+        n += d
+    ne.append(n)
+    for l in ll:
+        ne.append(d+str(l)+d)
+    ne.append(n)
+
+    n = []
+    if ne[x+1][y+0] != d:
+        n.append(ne[x+1][y+0])
+    else:
+        n.append(d)
+    if ne[x+0][y+1] != d:
+        n.append(ne[x+0][y+1])
+    else:
+        n.append(d)
+    if ne[x+1][y+2] != d:
+        n.append(ne[x+1][y+2])
+    else:
+        n.append(d)
+    if ne[x+2][y+1] != d:
+        n.append(ne[x+2][y+1])
+    else:
+        n.append(d)
+    return(n)
