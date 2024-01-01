0508cfb695913982cc0c983ba96df7fc3687171b
LayanJethwa <84594346+LayanJethwa@users.noreply.github.com>
2024-01-01 16:41:40 +0000

    Update aoc02.py

--- 2019/aoc02.py
+++ 2019/aoc02.py
@@ -1,5 +1,6 @@
 import math
 import adj
+import intcode
 ll = open("input.txt",'r').read().splitlines()
 for l in ll:
     l = l.split(',')
@@ -8,15 +9,9 @@ ol = l.copy()
 l[1] = 12
 l[2] = 2
 
-i = 0
-while l[i] != 99:
-    if l[i] == 1:
-        l[l[i+3]] = l[l[i+1]]+l[l[i+2]]
-    elif l[i] == 2:
-        l[l[i+3]] = l[l[i+1]]*l[l[i+2]]
-    i += 4
-print(l[0])
-        
+print(intcode.run(l)[0])
+
+
 
 
 
@@ -25,12 +20,5 @@ for n in range(100):
         l = ol.copy()
         l[1] = n
         l[2] = v
-
-        i = 0
-        while l[i] != 99:
-            if l[i] == 1:
-                l[l[i+3]] = l[l[i+1]]+l[l[i+2]]
-            elif l[i] == 2:
-                l[l[i+3]] = l[l[i+1]]*l[l[i+2]]
-            i += 4
-        if l[0] == 19690720: print((100*n)+v)
+        if intcode.run(l)[0] == 19690720:
+            print((100*n)+v)
